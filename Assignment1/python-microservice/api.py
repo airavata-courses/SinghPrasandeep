9008265f079d8934f5fde2d01247499a674c6c9f
Prasandeep Singh <prasan.ubhi@gmail.com>
2017-09-04 12:10:47 -0400

    Added interconnectivity amongst microservices

--- Assignment1/python-microservice/api.py
+++ Assignment1/python-microservice/api.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask
 from flask_cors import CORS
 
@@ -6,7 +7,14 @@ CORS(app)
 
 @app.route("/greeting")
 def say_hello():
-	return  "Hello Docker from Python!";
+	return "Hello Docker from Python!";
+
+@app.route("/call_java_node")
+def call_java_node():
+	r = requests.get("http://localhost:8080/hi")
+	s = requests.get("http://localhost:3000/api/sayhello")
+	return  "Response recieved from Java :: Response recieved from Node"
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
